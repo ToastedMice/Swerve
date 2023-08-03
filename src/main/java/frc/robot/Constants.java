ef3dfbd838b90ebab88074955e125867e2171072
ToastedMice <aidan.pawson1@icloud.com>
2023-08-03 20:24:08 +1000

    changed controller deadzone

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,6 +1,6 @@
 package frc.robot;
 public final class Constants {
-    
+
     public final static int pigeonID = 0;
-    public static final double stickDeadzone = 0.1;
+    public static final double stickDeadzone = 0.2;
 }
