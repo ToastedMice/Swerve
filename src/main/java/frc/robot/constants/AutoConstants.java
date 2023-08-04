3f2e7c157b1d38db3551ed482a1432194b87f1b4
ToastedMice <aidan.pawson1@icloud.com>
2023-08-04 17:57:34 +1000

    updated values

--- src/main/java/frc/robot/constants/AutoConstants.java
+++ src/main/java/frc/robot/constants/AutoConstants.java
@@ -8,10 +8,8 @@ public class AutoConstants {
     public static final double kPXController = 1;
     public static final double kPYController = 1;
     public static final double kPThetaController = 1;
-    public static final double kMaxAngularSpeedRadiansPerSecond = Math.PI;
-    public static final double kMaxAngularSpeedRadiansPerSecondSquared = Math.PI;
     
     public static final TrapezoidProfile.Constraints kThetaControllerConstraints =
             new TrapezoidProfile.Constraints(
-                kMaxAngularSpeedRadiansPerSecond, kMaxAngularSpeedRadiansPerSecondSquared);
+                6.28, 3.14);
 }
